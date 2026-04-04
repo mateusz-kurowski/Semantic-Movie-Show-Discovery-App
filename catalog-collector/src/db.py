0f19fa5662a4e91c6695c2f334d34b3c47d83442
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2026-04-04 22:05:59 +0200

    fix: Refactor imports and enhance batch insert logic with ON CONFLICT handling

--- catalog-collector/src/db.py
+++ catalog-collector/src/db.py
@@ -1,10 +1,10 @@
-from sqlalchemy.orm import declarative_base
-from sqlmodel import Field, SQLModel, create_engine, Session
-from env import get_envs
-from pydantic import BaseModel, ConfigDict
 import polars as pl
-from sqlalchemy.dialects.postgresql import insert
+from env import get_envs
 from movie import Movie
+from pydantic import BaseModel, ConfigDict
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.orm import declarative_base
+from sqlmodel import Field, Session, SQLModel, create_engine
 
 
 class MyModel(BaseModel):
@@ -46,8 +46,11 @@ def insert_movies_in_batches(df: pl.DataFrame, batch_size: int = 10_000):
             # Create a single bulk insert statement for the entire batch
             stmt = insert(Movie).values(records)
 
+            # Prepare the dictionary for the ON CONFLICT DO UPDATE clause
+            update_dict = {c.name: c for c in stmt.excluded}
+
             # Keep your safe insert logic entirely in SQL!
-            stmt = stmt.on_conflict_do_update(index_elements=["id"])
+            stmt = stmt.on_conflict_do_update(index_elements=["id"], set_=update_dict)
 
             # Execute the batch (1 query per 10,000 rows instead of 10,000 queries)
             session.exec(stmt)
