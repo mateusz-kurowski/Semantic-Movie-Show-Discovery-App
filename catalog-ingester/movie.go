2d775a23986f1cc99715b4e368bd649560679231
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2026-04-05 17:14:44 +0200

    feat: add semantic_text field to movie model and update VS Code Go linting and formatting settings

--- catalog-ingester/movie.go
+++ catalog-ingester/movie.go
@@ -57,6 +57,7 @@ func (m Movie) toMap() map[string]any {
 		"popularity":        m.Popularity,
 		"original_id":       m.ID,
 		"chunk_order":       m.ChunkOrder,
+		"semantic_text":     m.SemanticText,
 	}
 
 	if m.Title != nil {
