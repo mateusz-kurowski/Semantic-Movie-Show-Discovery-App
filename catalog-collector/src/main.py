5ecd10ec8c52fd594ace95266f8b45250c606124
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2026-07-02 22:37:26 +0200

    refactor: remove unnecessary blank line in main.py

--- catalog-collector/src/main.py
+++ catalog-collector/src/main.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 from env import get_envs
 from logger import log
 
-
 dotenv_path = os.path.join(os.path.dirname(__file__), "..", "..", ".env")
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
