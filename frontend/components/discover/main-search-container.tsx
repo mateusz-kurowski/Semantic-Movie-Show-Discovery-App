feeeca67373a415cdd87b76cddea1504fddb85d2
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2026-07-17 00:12:10 +0200

    feat: implement search form component with validation and integrate into main search container

--- frontend/components/discover/main-search-container.tsx
+++ frontend/components/discover/main-search-container.tsx
@@ -1,12 +1,12 @@
 "use client";
-import MainSearchComponent from "./main-search";
+import SearchForm from "./main-search";
 import PopularMovies from "./popuilar-movies";
 
 const MainSearchContainer = () => {
 	return (
 		<main className="flex flex-col items-center p-5 gap-5">
 			<h2 className="text-4xl font-bold">Find a film for any mood.</h2>
-			<MainSearchComponent />
+			<SearchForm />
 			<PopularMovies />
 		</main>
 	);
--- frontend/components/discover/main-search.tsx
+++ frontend/components/discover/main-search.tsx
@@ -1,6 +1,10 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ArrowRight } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
 import { Button } from "../ui/button";
-import { Field } from "../ui/field";
+import { Field, FieldGroup } from "../ui/field";
 import {
 	InputGroup,
 	InputGroupAddon,
@@ -9,23 +13,58 @@ import {
 import RecommendationBadges from "./recommendation-badges";
 import SearchModeContainer from "./search-mode-container";
 
-const MainSearchComponent = () => {
+const searchFormSchema = z.object({
+	query: z
+		.string()
+		.min(1, "Query is required")
+		.max(100, "Query must be less than 100 characters"),
+});
+
+const SearchForm = () => {
+	const form = useForm<z.infer<typeof searchFormSchema>>({
+		resolver: zodResolver(searchFormSchema),
+		defaultValues: {
+			query: "",
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof searchFormSchema>) => {
+		console.log(values);
+	};
+
+	const formId = "form-movies-search";
 	return (
-		<Field>
-			<InputGroup>
-				<InputGroupAddon align="inline-start">
-					<SearchModeContainer />
-				</InputGroupAddon>
-				<InputGroupInput placeholder="A hopeful sci-fi adventure about rebellion..." />
-				<InputGroupAddon align="inline-end">
-					<Button className="text-white">
-						<ArrowRight />
-					</Button>
-				</InputGroupAddon>
-			</InputGroup>
-			<RecommendationBadges />
-		</Field>
+		<form id={formId} className="w-full" onSubmit={form.handleSubmit(onSubmit)}>
+			<FieldGroup>
+				<Controller
+					name="query"
+					control={form.control}
+					render={({ field, fieldState }) => (
+						<Field data-invalid={fieldState.invalid}>
+							<InputGroup>
+								<InputGroupAddon align="inline-start">
+									<SearchModeContainer />
+								</InputGroupAddon>
+								<InputGroupInput
+									{...field}
+									id={field.name}
+									aria-invalid={fieldState.invalid}
+									placeholder="A hopeful sci-fi adventure about rebellion..."
+									autoComplete="off"
+								/>
+								<InputGroupAddon align="inline-end">
+									<Button className="text-white" type="submit" form={formId}>
+										<ArrowRight />
+									</Button>
+								</InputGroupAddon>
+							</InputGroup>
+							<RecommendationBadges />
+						</Field>
+					)}
+				/>
+			</FieldGroup>
+		</form>
 	);
 };
 
-export default MainSearchComponent;
+export default SearchForm;
